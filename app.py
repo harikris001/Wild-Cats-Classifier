8df3c52d3faea0d874c196e78f08a49fa9df11a1
harikris001 <hk5983835@gmail.com>
2023-04-29 13:07:52 +0530

    Update in UI

--- app.py
+++ app.py
@@ -65,12 +65,13 @@ def predict(model,img):
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
     return target_image_pred_label
     
-
+st.image('https://c1.wallpaperflare.com/preview/987/480/945/big-cats-collage-predators-animals.jpg',width=350)
 st.header('Wild Big Cat Classifier')
 st.write("Welcome to my project where you can explore the fascinating world of wild cats through the lens of cutting-edge deep learning technology. Trained  on different types of neural networks, including EfficientNet, Inception, and TinyVGG, on a comprehensive dataset of wild cats, enabling you to easily identify various feline species. Whether you're a wildlife enthusiast or simply a cat lover, you're sure to enjoy this exciting journey into the wild. So, join us and let's discover the beauty and diversity of wild cats together!")
 
 
 with st.sidebar:
+    st.image('https://miro.medium.com/v2/resize:fit:1400/1*3fA77_mLNiJTSgZFhYnU0Q.png')
     st.title('Choose Neural Network')
     selection = st.selectbox('Choose the Neural Network: ',options=('EfficientNet B0','Inception','TinyVGG'),help='Choose model for prediction')
     st.write('Test Accuracy of different Models:')
@@ -79,11 +80,12 @@ with st.sidebar:
     st.markdown("TinyVGG: 56%")
 
 if selection == 'EfficientNet B0':
+    st.sidebar.image('https://www.researchgate.net/publication/349299852/figure/fig3/AS:991096909869056@1613307325251/The-network-architecture-of-EfficientNet-It-can-output-a-feature-map-with-deep-semantic.jpg',caption='EfficientNet structure')
     st.markdown('#### EfficientNet B0')
     st.write('EfficientNet is a family of convolutional neural networks designed to achieve state-of-the-art accuracy with fewer parameters and faster inference times. EfficientNet models have been widely adopted in various computer vision tasks, such as image classification, object detection, and segmentation.')
 
     st.markdown('#### Upload Image')
-    file = st.file_uploader('choose Imge:',type=['jpg','jpeg','png'])
+    file = st.file_uploader('choose Image:',type=['jpg','jpeg','png'])
     if file:
         img = Image.open(file)
         transimg = eff_transform(img)
@@ -98,6 +100,7 @@ if selection == 'EfficientNet B0':
 
 
 if selection == 'Inception':
+    st.sidebar.image('https://www.researchgate.net/publication/349717475/figure/fig5/AS:996933934014464@1614698980419/The-architecture-of-Inception-V3-model.ppm',caption='Inception structure')
     st.markdown('#### Inception V3')
     st.write('Inception-v3 is a convolutional neural network architecture that was introduced by Google in 2015. It is a deep neural network with 48 layers that achieved state-of-the-art performance on the ImageNet Large Scale Visual Recognition Challenge (ILSVRC) classification task. Inception-v3 is known for its use of factorization in convolutional layers, which reduces the number of computations required during training and inference.')
 
@@ -117,6 +120,7 @@ if selection == 'Inception':
 
 
 if selection == 'TinyVGG':
+    st.sidebar.image('https://nnart.org/wp-content/uploads/2022/06/vgg-a-banner.webp',caption='TinyVGG structure')
     st.markdown('#### TinyVGG')
     st.write('TinyVGG is a compact version of the popular VGG network architecture, designed to achieve high accuracy with fewer parameters. It was introduced by researchers at the University of Oxford in 2017. TinyVGG consists of only a few layers compared to the original VGG, making it more efficient to train and faster to run on low-power devices.')
 
